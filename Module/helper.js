7db45d6cbe61c3dd0b5f7665876169f326c5f92c
saurav <sauravkumaragarwal7277@gmail.com>
2026-02-10 12:03:11 +0530

    Module added

---
+++ Module/helper.js
@@ -0,0 +1,5 @@
+export let x = 100;
+export function sum(a,b){
+    console.log(a+b);
+}
+// export {x};
---
+++ Module/script.js
@@ -0,0 +1,5 @@
+import {x,sum} from "./helper.js"
+import sub from "./defaultfile.js"
+console.log(x)
+sub(8,4);
+sum(5,10)
